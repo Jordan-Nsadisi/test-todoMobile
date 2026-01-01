b221187d12ddec8ef808d7607c8786bae7aca358
Jordan Nsadisi <astro17nsadisi@gmail.com>
2026-01-01 16:26:59 +0100

    feat: add central export file for improved module organization

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Main src exports - Central export file
+export * from './components';
+export * from './hooks';
+export * from './services';
+export * from './store';
+export * from './types';
+export * from './utils';
+export * from './constants';
