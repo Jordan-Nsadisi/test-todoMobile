cbc53ec3b903c26720107440eb1d85718bbd231f
Jordan Nsadisi <astro17nsadisi@gmail.com>
2026-01-01 16:28:07 +0100

    feat: add index file for component exports and future business components

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+// Components exports
+export { ThemedText } from './themed-text';
+export { ThemedView } from './themed-view';
+export { ExternalLink } from './external-link';
+export { HapticTab } from './haptic-tab';
+export { HelloWave } from './hello-wave';
+export { default as ParallaxScrollView } from './parallax-scroll-view';
+
+// Business components (to be added later)
+// export { TaskCard } from './TaskCard';
+// export { TaskModal } from './TaskModal';
+// export { LoadingSpinner } from './LoadingSpinner';
