67669dc8e2a17c2ba35248f42dbd92bd1a261d6a
Jordan Nsadisi <astro17nsadisi@gmail.com>
2026-01-01 22:13:56 +0100

    import task service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,4 @@
 // API services exports
 export { authService } from './auth';
 export { queryClient, queryKeys } from './queryClient';
-
-// Future services can be added here
-// export { taskService } from './tasks';
+export { taskService } from './tasks';
