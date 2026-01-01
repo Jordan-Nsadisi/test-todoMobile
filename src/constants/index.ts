c83f262f18468bfbbd834fed9eeba578689ae227
Jordan Nsadisi <astro17nsadisi@gmail.com>
2026-01-01 16:29:00 +0100

    feat: create index file for theme constants and additional exports

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+// Theme constants (moved from constants/theme.ts)
+export { Colors } from '../constants/theme';
+
+// Additional theme exports
+// export { Fonts } from '../constants/theme';
+// export { Spacing } from '../constants/theme';
+// export { BorderRadius } from '../constants/theme';
