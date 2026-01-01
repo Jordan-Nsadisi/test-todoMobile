74faaab1fa3d92e88b41c1511cb273fbe40e0e36
Jordan Nsadisi <astro17nsadisi@gmail.com>
2026-01-01 16:25:19 +0100

    fix import paths for components and hooks to use the correct directory structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { ThemedText } from '@/src/components/themed-text';
+import { ThemedView } from '@/src/components/themed-view';
 import { StyleSheet, View } from 'react-native';
 
 export default function DashboardScreen() {
