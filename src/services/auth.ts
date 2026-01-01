db3a30904b973ef19aa7f698ab436ef660f95d30
Jordan Nsadisi <astro17nsadisi@gmail.com>
2026-01-01 20:33:13 +0100

    add authService with login, register, logout, ... methods

---
+++ src/services/auth.ts
@@ -0,0 +1,45 @@
+import type {
+   AuthResponse,
+   LoginFormData,
+   RegisterFormData,
+   User
+} from '@/src/types';
+import { API_ENDPOINTS } from '../utils/endpoint';
+import { apiClient } from './api';
+
+
+export const authService = {
+
+   async login(credentials: LoginFormData): Promise<AuthResponse> {
+      const response = await apiClient.post<AuthResponse>(
+         API_ENDPOINTS.auth.login,
+         credentials
+      );
+      return response.data;
+   },
+
+   async register(userData: RegisterFormData): Promise<AuthResponse> {
+      const { confirmPassword, ...registerData } = userData;
+
+      const response = await apiClient.post<AuthResponse>(
+         API_ENDPOINTS.auth.register,
+         registerData
+      );
+      return response.data;
+   },
+
+   async logout(): Promise<void> {
+      await apiClient.post(API_ENDPOINTS.auth.logout);
+   },
+
+   async getCurrentUser(): Promise<User> {
+      const response = await apiClient.get<User>(API_ENDPOINTS.auth.getUser);
+      return response.data;
+   },
+
+ //pour rafraichir le token au besoin de l'authentification
+   async refreshToken(): Promise<AuthResponse> {
+      const response = await apiClient.post<AuthResponse>('/auth/refresh');
+      return response.data;
+   },
+};
