49489140baa0dfb6996a77b50755f1f4046cd2b0
Jordan Nsadisi <astro17nsadisi@gmail.com>
2026-01-02 01:01:57 +0100

    add route protection to AuthLayout component

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,10 @@
+import { useProtectedRoute } from '@/src/hooks';
 import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
+   //auto protection des routes
+   useProtectedRoute();
+
    return (
       <Stack screenOptions={{ headerShown: false }}>
          <Stack.Screen name="login" />
