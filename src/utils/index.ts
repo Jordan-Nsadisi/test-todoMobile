9cf293dec0d02706f63ea6a61eb57fa6b77bb1a8
Jordan Nsadisi <astro17nsadisi@gmail.com>
2026-01-02 01:51:02 +0100

    fix: correct comment formatting and clean up unused exports in utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
-// Utility exports
+//utility exports
 export * from './designSystem';
-// export { API_ENDPOINTS } from './constants';
-// export { globalStyles } from './styles';
-
-// Future utils can be added here
+export * from './endpoint';
+export * from './navigation';
